3fe53f229f8b78c8e45a781a210fb2ce16086df7
Greg <gregory.degruy@gmail.com>
2020-02-11 16:53:41 -0800

    + form event handlers and state capture ! buttonClick references undefined state

--- AuthenticationComponent/AuthenticationComponent/AuthenticationForm.tsx
+++ AuthenticationComponent/AuthenticationComponent/AuthenticationForm.tsx
@@ -1,56 +1,72 @@
 import * as React from "react";
-import { DefaultButton, 
-	IStackTokens,
+import { 
+	DefaultButton, 
+	IStackProps,
 	Stack, 
 	TextField
 } from 'office-ui-fabric-react';
-import { PersonaSize } from "office-ui-fabric-react/lib/Persona";
 
-const stackTokens: IStackTokens = { childrenGap: 40 };
+const columnProps: Partial<IStackProps> = {
+	tokens: { childrenGap: 15 },
+	styles: { root: { width: 600 } }
+};
 
-export class AuthenticationForm extends React.Component {
+export interface ITextFieldControlledExampleState {
+	username: string;
+	password: string;
+}
+
+export class AuthenticationForm extends React.Component<{}, ITextFieldControlledExampleState> {
+	
+	public state: ITextFieldControlledExampleState = { username: '', password: '' };
+	
 	constructor(props: any) {
-		super(props);
-
-		this.state = {
-			numberOfFaces: props.numberOfFaces || 3,
-			imagesFadeIn: true,
-			personaSize: PersonaSize.size32
-		};
-	}
-
-	public componentWillReceiveProps(newProps: any): void {
-		this.setState(newProps);
+		super(props)
 	}
 
 	public render(): JSX.Element {
-		return (
-			<div className={"msFacepileExample"}>
-				<h3>React version in control: {React.version}</h3>
-				<h3>React version in host window: {(window as any).React.version}</h3>
-				<div className={"control"}>
-					<Stack horizontal tokens={stackTokens}>
-						<TextField 
-							label="Username" 
-							type="text"
-						/>
-						<TextField 
-							label="Password"
-							type="password" 
-						/>
-						<DefaultButton 
-							text="Submit" 
-							onClick={_alertClicked} 
-							allowDisabledFocus 
-						/>
-					</Stack>
+		return (			
+			<>
+				<div className="ms-Grid" dir="rtl">
+					<div className="ms-Grid-row">
+						<h3>React version in control: {React.version}</h3>
+						<h3>React version in host window: {(window as any).React.version}</h3>
+						<div className={"control"}>
+							<Stack {...columnProps}>
+								<TextField 
+									label="Username" 
+									type="text"
+									value={this.state.username}
+									onChange={this.usernameOnChange}
+									/>
+								<TextField 
+									label="Password"
+									type="password" 
+									value={this.state.password}
+									onChange={this.passwordOnChange}
+								/>
+								<DefaultButton 
+									text="Submit" 
+									onClick={this.buttonClick} 
+									allowDisabledFocus 
+								/>
+							</Stack>
+						</div>
+					</div>
 				</div>
-			</div>
+			</>
 		);
 	}
 
-}
-
-function _alertClicked(): void {
-	alert('Clicked');
+	// onChange?: (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => void;
+	private usernameOnChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+		this.setState({ username: newValue || '' });
+	};
+	private passwordOnChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+		this.setState({ password: newValue || '' });
+	};
+	private buttonClick(): void {
+		alert('Clicked with user' + this.state.username + 'and pas ' + this.state.password);
+	
+	};
 }
