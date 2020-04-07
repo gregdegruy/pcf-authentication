7cca90cc1a023c3a3d3da59988374d47be2a64fe
Greg <gregory.degruy@gmail.com>
2020-04-06 23:50:42 -0700

    + sha1 has test

--- CDS/CDS.CustomControl.Encryption.Test/UnitTest.cs
+++ CDS/CDS.CustomControl.Encryption.Test/UnitTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
 using Xunit;
 
 namespace CDS.CustomControl.Encryption.Test
@@ -6,9 +8,29 @@ namespace CDS.CustomControl.Encryption.Test
     public class UnitTest
     {
         [Fact]
-        public void Test1()
+        public void CreateCDSUserHash()
         {
+            string cdsUserToken = "";
+            string systemUserId = "00000000-0000-0000-0000-000000000000";
+            string internalemailaddress = "supermario@mushroom.kingdom";
+            string azureActiveDirectoryObjectId = "00000000-0000-0000-0000-000000000001";
+            string input = "";
+            input += systemUserId;
+            input += internalemailaddress;
+            input += azureActiveDirectoryObjectId;
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
 
+                foreach (byte b in hash)
+                {
+                    // "x2" for lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+                cdsUserToken = sb.ToString();
+            }
+            Assert.Equal("E1FCDE486F3A17D64C33889AB9EDD3681E4FA40C", cdsUserToken);
         }
     }
 }
